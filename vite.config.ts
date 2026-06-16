747d5d17a25484ac09d17092bbe9d81c3eff23d0
uriah-source <aparajoshua@gmail.com>
2026-06-16 04:11:34 +0100

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+export default defineConfig({
+  plugins: [
+    tsConfigPaths(),
+    tailwindcss(),
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    tanstackStart({ server: { preset: "netlify" } }),
+    react(),
+  ],
+});
